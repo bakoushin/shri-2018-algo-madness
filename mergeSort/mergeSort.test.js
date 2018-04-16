6607d9e10d15e5effaa0a5b46707a88736588765
Alex Bakoushin <alex@bakoush.in>
2018-04-17 00:10:33 +0300

    Refactor tests

--- mergeSort/mergeSort.test.js
+++ mergeSort/mergeSort.test.js
@@ -1,21 +1,3 @@
-const {expect} = require('chai');
-const randomArray = require('random-array');
-const sortFunction = require('./mergeSort');
+const sortTest = require('../test/sort');
 
-describe('Merge sort', () => {
-
-  it('Sould sort random array of numbers correctly', () => {
-    const sampleArray = randomArray(1, 100).oned(100, {round: true});
-    const testArray = sortFunction(sampleArray);
-    sampleArray.sort((a, b) => a - b);
-    expect(testArray).to.be.deep.equal(sampleArray);
-  });
-
-  it("Doesn't break already sorted array", () => {
-    const sampleArray = randomArray(1, 100).oned(100, {round: true})
-      .sort((a, b) => a - b);
-    const testArray = sortFunction(sampleArray);
-    expect(testArray).to.be.deep.equal(sampleArray);
-  });
-
-});
+describe('Merge sort', () => sortTest(require('./mergeSort')));
