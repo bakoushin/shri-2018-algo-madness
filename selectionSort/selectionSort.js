71a1ad2019155c2a9a080decab9b99c7fda8517b
Alex Bakoushin <alex@bakoush.in>
2018-04-18 17:52:55 +0300

    Fix naming and add comments

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -1,14 +1,14 @@
 function selectionSort(array) {
   let pointer = array.length - 1;
   while(pointer > 0) {
-    let biggestIndex = pointer;
+    let biggestItemIndex = pointer;
     for (let i = 0; i < pointer; i++) {
-      if (array[i] > array[biggestIndex]) {
-        biggestIndex = i;
+      if (array[i] > array[biggestItemIndex]) {
+        biggestItemIndex = i;
       }
     }
-    const item = array.splice(biggestIndex, 1)[0];
-    array.splice(pointer, 0, item);
+    const item = array.splice(biggestItemIndex, 1)[0]; // remove element from its position
+    array.splice(pointer, 0, item); // insert into sorted part of array
     pointer--;
   }
   return array;
