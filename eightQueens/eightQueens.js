912c7e0418146be144b07d385d64f90b9ade957a
Alex Bakoushin <alex@bakoush.in>
2018-04-19 20:45:42 +0300

    Implement backtracking to find all 92 variants

--- eightQueens/eightQueens.js
+++ eightQueens/eightQueens.js
@@ -1,49 +1,53 @@
 const Board = require('./Board');
 
-function eightQueens() {
+function eightQueens(verbose = false) {
   const BOARD_WIDTH = 8;
   const BOARD_HEIGHT = 8;
   const QUEENS_NUMBER = 8;
 
-  let board = new Board(BOARD_WIDTH, BOARD_HEIGHT);
+  let result = [];
 
-  for (let row = 0; row < BOARD_HEIGHT; row++) {
-    for (let column = 0; column < BOARD_WIDTH; column++) {
-      board.placeQueen(row, column);
-      placeQueens(board);
-      // let emptyCell = [row, column];
-      // while(emptyCell) {
-      //   const [row, column] = emptyCell;
-      //   board.placeQueen(row, column);
-      //   if (board.countQueens() === QUEENS_NUMBER) {
-      //     return board;          
-      //   }
-      //   emptyCell = board.findEmptyCell();
-      // }
-      board.clear();
-    }
+  for (let column = 0; column < BOARD_WIDTH; column++) {
+    let board = new Board(BOARD_WIDTH, BOARD_HEIGHT);
+    board.placeQueen(0, column);
+    placeQueens(board);
   }
 
   function placeQueens(board) {
     const emptyCells = board.findEmptyCells();
     for (const emptyCell of emptyCells) {
       const [row, column] = emptyCell;
-      const newBoard = Object.assign(new Board(), board);
+      const newBoard = board.clone();
       newBoard.placeQueen(row, column);
       if (newBoard.countQueens() === QUEENS_NUMBER) {
-        console.log(board.toString());
-        console.log('--------------')
+        const solution = newBoard.toString();
+        if (!result.includes(solution)) {
+          result.push(solution);
+          if (verbose) {
+            console.log(newBoard.toString());
+            console.log('---------------', result.length);
+          }
+        }
       }
       placeQueens(newBoard);
     }
-
   }
 
-  
-
-  return null;
+  return result;
 }
 
+function timer(start) {
+  if (!start) return process.hrtime();
+  const result = process.hrtime(start);
+  return Math.round(result[0] + result[1] / 1000000000);
+}
+const start = timer();
+
 eightQueens();
+
+const diff = timer(start);
+const min = Math.floor(diff / 60);
+const sec = Math.round(diff % 60);
+console.log(`Total time: ${min} min ${sec} sec`);
 
 module.exports = eightQueens;
