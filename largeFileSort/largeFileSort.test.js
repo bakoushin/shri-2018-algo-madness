9b1a7c6915c8c531f48bd0e42d5e01f3acbc60e4
Alex Bakoushin <alex@bakoush.in>
2018-04-21 01:59:29 +0300

    Tidy up

--- largeFileSort/largeFileSort.test.js
+++ largeFileSort/largeFileSort.test.js
@@ -7,7 +7,6 @@ describe('Large file sort', () => {
 
   it('Should sort file with random numbers correctly', () => {
     const sampleArray = randomArray(1, 100).oned(100, {round: true});
-    console.log(sampleArray)
     const file = new File(sampleArray);
     sampleArray.sort((a, b) => a - b);
     expect(largeFileSort(file).toArray()).to.be.deep.equal(sampleArray);
@@ -16,9 +15,8 @@ describe('Large file sort', () => {
   it("Doesn't break already sorted file", () => {
     const sampleArray = randomArray(1, 100).oned(100, {round: true})
       .sort((a, b) => a - b);
-      console.log(sampleArray)
-      const file = new File(sampleArray);
-   expect(largeFileSort(file).toArray()).to.be.deep.equal(sampleArray);
+    const file = new File(sampleArray);
+    expect(largeFileSort(file).toArray()).to.be.deep.equal(sampleArray);
   });
 
 });
